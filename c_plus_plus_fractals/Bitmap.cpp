1c357e1198137433707704bcfe156734b9a42cef
James <jakub.krogulec5@gmail.com>
2018-07-17 19:41:42 +0200

    Added test pixel write implementation for changing pixel colors

--- c_plus_plus_fractals/Bitmap.cpp
+++ c_plus_plus_fractals/Bitmap.cpp
@@ -2,6 +2,7 @@
 #include "Bitmap.h"
 #include "bitmapInfoHeader.h"
 #include "bitmapFileHeader.h"
+#include <fstream>
 
 
 Bitmap::Bitmap(int width, int height) : m_width(width), m_height(height), m_pixels(new uint8_t[width * height * 3]{}) {
@@ -10,6 +11,7 @@ Bitmap::Bitmap(int width, int height) : m_width(width), m_height(height), m_pixe
 
 
 Bitmap::~Bitmap() {
+
 }
 
 
@@ -23,10 +25,32 @@ bool Bitmap::write(std::string filename) {
 	infoHeader.width = m_width;
 	infoHeader.height = m_height;
 
-	return false;
+	std::ofstream file;
+	file.open(filename, std::ios::out | std::ios::binary);
+
+	if (!file) {
+		return false;
+	}
+
+	file.write((char *)&fileHeader, sizeof(fileHeader));
+	file.write((char *)&infoHeader, sizeof(infoHeader));
+	file.write((char *)m_pixels.get(), m_width * m_height * 3);
+
+	file.close();
+
+	if (!file) {
+		return false;
+	}
+
+	return true;
 }
 
 
 void Bitmap::setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue) {
+	uint8_t *pixel = m_pixels.get();
 
+	pixel += (y * 3) * m_width + (x * 3);
+	pixel[0] = blue;
+	pixel[1] = green;
+	pixel[2] = red;
 }
